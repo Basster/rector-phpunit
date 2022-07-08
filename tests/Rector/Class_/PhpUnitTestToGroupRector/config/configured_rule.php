53d408345b1995b2ad8e6a7ce96235d33253afcc
GitHub Action <action@github.com>
2022-07-08 09:42:34 +0000

    [ci-review] Rector Rectify

--- tests/Rector/Class_/PhpUnitTestToGroupRector/config/configured_rule.php
+++ tests/Rector/Class_/PhpUnitTestToGroupRector/config/configured_rule.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Basster\Rector\PhpUnit\Rector\Class_\PhpUnitTestToGroupRector;
+
 use Basster\Rector\PhpUnit\ValueObject\PhpUnitTestToGroup;
 use Rector\Config\RectorConfig;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(
         PhpUnitTestToGroupRector::class,
-        [new PhpUnitTestToGroup('kernel', '\Symfony\Bundle\FrameworkBundle\Test\KernelTestCase')]
+        [new PhpUnitTestToGroup('kernel', KernelTestCase::class)]
     );
 };
--- tests/Rector/Class_/PhpUnitTestToGroupRector/config/interface_configured_rule.php
+++ tests/Rector/Class_/PhpUnitTestToGroupRector/config/interface_configured_rule.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Test\AppTestInterface;
+
 use Basster\Rector\PhpUnit\Rector\Class_\PhpUnitTestToGroupRector;
 use Basster\Rector\PhpUnit\ValueObject\PhpUnitTestToGroup;
 use Rector\Config\RectorConfig;
@@ -9,6 +11,6 @@ use Rector\Config\RectorConfig;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(
         PhpUnitTestToGroupRector::class,
-        [new PhpUnitTestToGroup('foobar', '\App\Test\AppTestInterface')]
+        [new PhpUnitTestToGroup('foobar', AppTestInterface::class)]
     );
 };
